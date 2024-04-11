b083fa4a82a5c200f1d5f187ebcdeae7f34ce4b5
ChristianDuarteR <christianjavierduarte@gmail.com>
2024-04-11 17:18:09 -0500

    Algunos graficos mas

--- graphs.py
+++ graphs.py
@@ -46,22 +46,17 @@ def plot_brand_distribution(data):
     plt.show()
 
 def plot_color_distribution(data):
-    # Extraer la columna de color del conjunto de datos
     colors = [row['color'] for row in data]
 
-    # Contar la frecuencia de cada color
     color_counts = {}
     for color in colors:
         color_counts[color] = color_counts.get(color, 0) + 1
 
-    # Ordenar los colores por frecuencia descendente
     sorted_colors = sorted(color_counts.items(), key=lambda x: x[1], reverse=True)
 
-    # Extraer los colores y las frecuencias ordenadas
     colors_sorted = [color[0] for color in sorted_colors]
     counts_sorted = [color[1] for color in sorted_colors]
 
-    # Crear el gráfico de barras
     plt.figure(figsize=(10, 6))
     sns.barplot(x=colors_sorted, y=counts_sorted, palette='pastel')
     plt.title('Distribución de Colores de Vehículos')
@@ -72,28 +67,22 @@ def plot_color_distribution(data):
     plt.show()
 
 def plot_engine_size_by_brand(data):
-    # Crear un diccionario para almacenar los tamaños de motor promedio por marca
     engine_size_by_brand = {}
 
-    # Iterar sobre los datos para calcular el tamaño de motor promedio para cada marca
     for row in data:
         brand = row['brand']
-        engine_size = float(row['engine_size'][:-1])  # Eliminar la unidad ('L') y convertir a flotante
+        engine_size = float(row['engine_size'][:-1])
         if brand not in engine_size_by_brand:
             engine_size_by_brand[brand] = []
         engine_size_by_brand[brand].append(engine_size)
 
-    # Calcular el tamaño de motor promedio para cada marca
     avg_engine_size_by_brand = {brand: sum(engine_sizes) / len(engine_sizes) for brand, engine_sizes in engine_size_by_brand.items()}
 
-    # Ordenar las marcas por tamaño de motor promedio descendente
     sorted_brands = sorted(avg_engine_size_by_brand.items(), key=lambda x: x[1], reverse=True)
 
-    # Extraer las marcas y los tamaños de motor promedio ordenados
     brands_sorted = [brand[0] for brand in sorted_brands]
     engine_sizes_sorted = [brand[1] for brand in sorted_brands]
 
-    # Crear el gráfico de barras
     plt.figure(figsize=(10, 6))
     sns.barplot(x=brands_sorted, y=engine_sizes_sorted, palette='husl')
     plt.title('Tamaño Promedio del Motor por Marca de Vehículo')
@@ -104,21 +93,17 @@ def plot_engine_size_by_brand(data):
     plt.show()
 
 def plot_fuel_type_distribution(data):
-    # Crear un diccionario para almacenar la frecuencia de cada tipo de combustible
     fuel_type_counts = {}
 
-    # Contar la frecuencia de cada tipo de combustible
     for row in data:
         fuel_type = row['fuel_Type']
         if fuel_type not in fuel_type_counts:
             fuel_type_counts[fuel_type] = 0
         fuel_type_counts[fuel_type] += 1
 
-    # Extraer los tipos de combustible y sus frecuencias
     fuel_types = list(fuel_type_counts.keys())
     counts = [fuel_type_counts[fuel_type] for fuel_type in fuel_types]
 
-    # Crear el gráfico de barras
     plt.figure(figsize=(8, 6))
     sns.barplot(x=fuel_types, y=counts, palette='hsv')
     plt.title('Distribución de Tipos de Combustible')
